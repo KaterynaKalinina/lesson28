74b52e9a2a734bd46ae7aba287f648428e7d3ade
X <sersergiusx@gmail.com>
2020-10-19 21:22:07 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const myList = [
+  {
+    value: 'First'
+  },
+  {
+    value: 'First'
+  },  {
+    value: 'First'
+  },  {
+    value: 'First'
+  }
+]
+
+function App() {
+  const [list, setList] = useState([myList]);
+  return (
+    <div className="App">
+      <ul>
+        {myList.map(listElement => (
+           <li>{listElement.value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
